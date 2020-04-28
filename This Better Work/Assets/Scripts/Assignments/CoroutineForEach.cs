8265297e13d6663381cfe374230adf78ed313302
Stefani <stefani.olson@gmail.com>
2020-04-27 20:21:22 -0500

    added coroutines

---
+++ This Better Work/Assets/Scripts/Assignments/CoroutineForEach.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineForEach : MonoBehaviour
+{
+    void Start()
+    {
+        string[] strings = new string[3];
+
+        strings[0] = "First string";
+        strings[1] = "Second string";
+        strings[2] = "Third string";
+
+        foreach (string item in strings)
+        {
+            print(item);
+            print("For Each is working!");
+        }
+    }
+}
---
+++ This Better Work/Assets/Scripts/Assignments/CoroutineWhile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineWhile : MonoBehaviour
+{
+    Coroutine LookAtCoroutine;
+
+    public void StartLookAt()
+    {
+        if (LookAtCoroutine != null)
+        {
+            StopCoroutine(LookAtCoroutine);
+
+        }
+        LookAtCoroutine = StartCoroutine(LookAt());
+    }
+
+    IEnumerator LookAt()
+    {
+        var duration = Time.time + 4.0f;
+        while (duration > Time.time)
+
+        {
+
+            print("the coroutine is working");
+            yield return null;
+        }
+    }
+}
